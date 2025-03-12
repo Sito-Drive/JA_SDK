8681d99bc01705112ffa17be227de272ed695f8b
ma-wei-jie <maweijie@jacode.cn>
2025-03-12 19:53:35 +0800

    Update ja_multi_motor_control.cpp

--- JA_485/C++/Linux/src/ja_multi_motor_control.cpp
+++ JA_485/C++/Linux/src/ja_multi_motor_control.cpp
@@ -21,7 +21,8 @@ void MultiMotorControl:: enable(const bool& value)
     {
         MotorControl:: set_object_id(id);
         MotorControl:: enable(value);
-        this_thread::sleep_for(chrono::microseconds(1));
+        // this_thread::sleep_for(chrono::microseconds(100));
+        sleep_ms(3);
     }
 }
 
@@ -34,7 +35,8 @@ void MultiMotorControl:: write_position(const vector<int32_t>& pos_value)
         int32_t Pos_value = pos_value.size() == 1 ? pos_value[0] : pos_value[i];
         MotorControl:: set_object_id(id);
         MotorControl:: write_position(Pos_value);
-        this_thread::sleep_for(chrono::microseconds(1));
+        // this_thread::sleep_for(chrono::microseconds(100));
+        sleep_ms(3);
     }
 }
 
@@ -47,7 +49,8 @@ void MultiMotorControl:: write_velocity(const vector<int16_t>& vel_value)
         int32_t Vel_value = vel_value.size() == 1 ? vel_value[0] : vel_value[i];
         MotorControl:: set_object_id(id);
         MotorControl:: write_velocity(Vel_value);
-        this_thread::sleep_for(chrono::microseconds(1));
+        // this_thread::sleep_for(chrono::microseconds(100));
+        sleep_ms(3);
     }
 }
 
@@ -60,7 +63,8 @@ void MultiMotorControl:: write_current(const vector<int16_t>& cur_value)
         int32_t Cur_value = cur_value.size() == 1 ? cur_value[0] : cur_value[i];
         MotorControl:: set_object_id(id);
         MotorControl:: write_current(Cur_value);
-        this_thread::sleep_for(chrono::microseconds(1));
+        // this_thread::sleep_for(chrono::microseconds(100));
+        sleep_ms(3);
     }
 }
 
@@ -73,7 +77,8 @@ void MultiMotorControl:: change_parameters(const SETTINGS address, const vector<
         int32_t Value = value.size() == 1 ? value[0] : value[i];
         MotorControl:: set_object_id(id);
         MotorControl:: change_parameters(address, Value);
-        this_thread::sleep_for(chrono::microseconds(1));
+        // this_thread::sleep_for(chrono::microseconds(100));
+        sleep_ms(3);
     }
 }
 
@@ -84,7 +89,8 @@ void MultiMotorControl:: stop()
         uint8_t id = motor_ids_[i];
         MotorControl:: set_object_id(id);
         MotorControl:: stop();
-        this_thread::sleep_for(chrono::microseconds(1));
+        // this_thread::sleep_for(chrono::microseconds(100));
+        sleep_ms(3);
     }
 }
 
@@ -95,7 +101,8 @@ void MultiMotorControl:: clear()
         uint8_t id = motor_ids_[i];
         MotorControl:: set_object_id(id);
         MotorControl:: clear();
-        this_thread::sleep_for(chrono::microseconds(1));
+        // this_thread::sleep_for(chrono::microseconds(100));
+        sleep_ms(3);
     }
 }
 
@@ -106,7 +113,8 @@ void MultiMotorControl:: set_origin()
         uint8_t id = motor_ids_[i];
         MotorControl:: set_object_id(id);
         MotorControl:: set_origin();
-        this_thread::sleep_for(chrono::microseconds(1));
+        // this_thread::sleep_for(chrono::microseconds(100));
+        sleep_ms(3);
     }
 }
 
@@ -117,7 +125,8 @@ void MultiMotorControl:: return_origin()
         uint8_t id = motor_ids_[i];
         MotorControl:: set_object_id(id);
         MotorControl:: return_origin();
-        this_thread::sleep_for(chrono::microseconds(1));
+        // // this_thread::sleep_for(chrono::microseconds(100));
+        sleep_ms(3);
     }
 }
 
@@ -128,7 +137,8 @@ void MultiMotorControl:: save()
         uint8_t id = motor_ids_[i];
         MotorControl:: set_object_id(id);
         MotorControl:: save();
-        this_thread::sleep_for(chrono::microseconds(1));
+        // this_thread::sleep_for(chrono::microseconds(100));
+        sleep_ms(3);
     }
 }
 
@@ -140,7 +150,8 @@ vector<MotorStatus> MultiMotorControl:: read(const bool& flag)
         uint8_t id = motor_ids_[i];
         MotorControl:: set_object_id(id);
         status.push_back(MotorControl:: read(flag));
-        this_thread::sleep_for(chrono::microseconds(1));
+        // this_thread::sleep_for(chrono::microseconds(100));
+        // sleep_ms(3);
     }
     return status;
 }
@@ -152,7 +163,7 @@ void MultiMotorControl:: brake(const bool& flag)
         uint8_t id = motor_ids_[i];
         MotorControl:: set_object_id(id);
         MotorControl:: brake(flag);
-        this_thread::sleep_for(chrono::microseconds(1));
+        this_thread::sleep_for(chrono::microseconds(10));
     }
 }
 
@@ -164,7 +175,7 @@ void MultiMotorControl:: write_time_position(const vector<int>& initial_position
     {
         int32_t initial_position = initial_positions.size() == 1 ? initial_positions[0] : initial_positions[i];
         int32_t target_position = target_positions.size() == 1 ? target_positions[0] : target_positions[i];
-        pos = trajectory(initial_position, target_positions[i], Hz, duration, num_points);
+        pos = trajectory(initial_position, target_positions[i], duration, num_points);
         positions.push_back(pos);
     }
 
